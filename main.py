498f0d8f1258c74f9edc300434a7c9160ec66a02
Ivan <st.affeev@yandex.ru>
2022-02-05 18:05:00 +0300

    ui added

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 import sys
 import requests
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 
 SCREEN_SIZE = [600, 450]
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('system_files/main.ui', self)
+        self.setWindowTitle('Отображение карты')
+        self.map_file = None
+        self.spn = self.z = self.ll = 0
+        self.map_type = 'map'
         self.getImage()
-        self.initUI()
 
     def getImage(self):
         map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
@@ -28,16 +37,7 @@ class Example(QMainWindow):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.picture.setPixmap(QPixmap(self.map_file))
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
@@ -48,4 +48,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
